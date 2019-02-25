48a5ad7486c4a62536d599bf603949849a1add85
Nick Mallory <nick.mallory@ontario.ca>
2019-02-25 10:01:01 -0500

    managed identity

--- SQLApplication/SQLApplication/Controllers/HomeController.cs
+++ SQLApplication/SQLApplication/Controllers/HomeController.cs
@@ -4,50 +4,98 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlClient;
+using Microsoft.Azure.Services.AppAuthentication;
+using System.Web.Configuration;
 
 namespace SQLApplication.Controllers
 {
-    public class HomeController : Controller
+    public struct Record
     {
+        public string Name;
+        public int ID;
+    }
+
+    public class HomeController : Controller
+    { 
         public ActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Connect(string connectionInput)
+        public ActionResult Connect(string connectionInput, bool? useIdentity)
         {
-            var connectionString = connectionInput;
-
-            using (SqlConnection con = new SqlConnection(connectionString))
+            bool identity = useIdentity != null ? useIdentity.Value : false;
+            if(identity)
             {
-                try
+                using (SqlConnection con = new SqlConnection())
                 {
-                    con.Open();
-                }
-                catch(SqlException e)
-                {
-                    List<string> messages = new List<string>();
-                    for(int i = 0; i < e.Errors.Count; i++)
+                    con.ConnectionString = connectionInput;
+                    con.AccessToken = (new AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net").Result;
+                    try
                     {
-                        messages.Add(e.Errors[i].Message);
+                        con.Open();
                     }
-                    ViewBag.Error = messages;
-                    return View();
-                }
-                
+                    catch (SqlException e)
+                    {
+                        List<string> messages = new List<string>();
+                        for (int i = 0; i < e.Errors.Count; i++)
+                        {
+                            messages.Add(e.Errors[i].Message);
+                        }
+                        ViewBag.Error = messages;
+                        return View();
+                    }
 
-                using (SqlCommand command = new SqlCommand("SELECT * FROM Test", con))
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    List<string> messages = new List<string>();
-                    while (reader.Read())
+
+                    using (SqlCommand command = new SqlCommand("SELECT * FROM Test", con))
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        messages.Add(reader.GetString(1));
+                        List<Record> messages = new List<Record>();
+                        while (reader.Read())
+                        {
+                            Record r;
+                            r.ID = reader.GetInt32(0);
+                            r.Name = reader.GetString(1);
+                            messages.Add(r);
+                        }
+                        ViewBag.Results = messages;
                     }
-                    ViewBag.Results = messages;
-                }               
+                }
             }
+            else
+            {
+                using (SqlConnection con = new SqlConnection(connectionInput))
+                {
+                    try
+                    {
+                        con.Open();
+                    }
+                    catch (SqlException e)
+                    {
+                        List<string> messages = new List<string>();
+                        for (int i = 0; i < e.Errors.Count; i++)
+                        {
+                            messages.Add(e.Errors[i].Message);
+                        }
+                        ViewBag.Error = messages;
+                        return View();
+                    }
+
+
+                    using (SqlCommand command = new SqlCommand("SELECT * FROM Test", con))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<string> messages = new List<string>();
+                        while (reader.Read())
+                        {
+                            messages.Add(reader.GetString(1));
+                        }
+                        ViewBag.Results = messages;
+                    }
+                }
+            }
+           
 
             return View();
         }
